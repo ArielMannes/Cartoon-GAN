cbb21eb1af045f714d4cf74e62a221ba1b50b693
AdiM32 <adididiadidas@gmail.com>
2019-04-16 16:37:07 +0300

    start slice video

--- Utils/SliceVideo.py
+++ Utils/SliceVideo.py
@@ -1,16 +1,46 @@
 import argparse
-
 import cv2
+from skimage.measure import compare_ssim as ssim
+from PIL import Image
+# import math
+# import numpy as np
+
+path_out = ''
+frame_count = 0
 
 
-def extract_images(path_in, path_out):
+def extract_images(path_in):
+    global path_out, frame_count
     vidcap = cv2.VideoCapture(path_in)
-    count, success = 0, True
-    while success and count < 10:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 1000))
+    success, last_image = vidcap.read()
+    count = 0
+    while success: # and frame_count < 100:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, count * 100)
         success, image = vidcap.read()
-        cv2.imwrite(path_out + '/frame{}.jpg'.format(count), image)
+        if image is None:
+            break
+        last_image = compare_and_save(last_image, image)
         count += 1
+
+
+def compare_and_save(imageA, imageB):
+    global path_out, frame_count
+    _ssim = ssim(imageA, imageB, multichannel=True)
+    # _psnr = psnr(imageA, imageB)
+    if _ssim < 0.35:
+        _image = Image.fromarray(imageB[:, :, ::-1], 'RGB')
+        _image = _image.resize((256, 256), Image.ANTIALIAS)
+        _image.save('{}/frame{}.jpg'.format(path_out, frame_count))
+        frame_count += 1
+        return imageB
+    return imageA
+
+
+# def psnr(imageA, imageB):
+#     mse = np.mean((imageA - imageB) ** 2)
+#     if mse == 0:
+#         return 100
+#     return 20 * math.log10(255.0 / math.sqrt(mse))
 
 
 if __name__ == "__main__":
@@ -20,4 +50,5 @@ if __name__ == "__main__":
     parser.add_argument('--pathIOut', help='path to save the frames',
                         default='../Spirited_Away_frames')
     args = parser.parse_args()
-    extract_images(args.pathIn, args.pathIOut)
+    path_out = args.pathIOut
+    extract_images(args.pathIn)
