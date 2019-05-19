71e6b6bafdd604224ebde8945a65b485a8f4b93f
ArielMannes <arielmannes@gmail.com>
2019-05-19 17:14:50 +0300

    Delete check.py

--- check.py
+++
@@ -1,8 +0,0 @@
-from CartoonGan import CartoonGAN
-
-all = CartoonGAN('')
-generator = all.generator()
-print(generator.summary())
-
-descriminator = all.discriminator()
-print(descriminator.summary())
